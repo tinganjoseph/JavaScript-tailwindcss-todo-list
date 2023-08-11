630106371e77def1994b5e46616c1f03a0868355
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-11 08:48:11 +0000

    creating script.js

---
+++ src/script.js
@@ -0,0 +1,36 @@
+let todos = [];
+
+const todoList = document.getElementById('todoList');
+const todoInput = document.getElementById('todoInput');
+
+function renderTodos() {
+  todoList.innerHTML = '';
+  todos.forEach((todo, index) => {
+    const todoItem = document.createElement('div');
+    todoItem.className = 'p-4 m-2 border bg-white rounded shadow';
+    
+    const todoText = `
+      <p class="text-xl">${todo}</p>
+      <div class="mt-4 flex justify-end">
+        <button onclick="deleteTodo(${index})" class="bg-red-500 text-white p-1 rounded">Delete</button>
+      </div>`;
+    todoItem.innerHTML = todoText;
+    todoList.appendChild(todoItem);
+  });
+}
+
+function addTodo() {
+  const value = todoInput.value;
+  if (value) {
+    todos.push(value);
+    todoInput.value = '';
+    renderTodos();
+  }
+}
+
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  renderTodos();
+}
+
+renderTodos();
