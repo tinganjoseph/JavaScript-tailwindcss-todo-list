c2c25c52f9bca7d2ed64bfe138b9427f6181fc0f
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-11 09:18:39 +0000

    error: debugging and solving the tailwind css issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.js'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
